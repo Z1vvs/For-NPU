77f0248a90bc0d56590b94ce9ec5b4d1b6496aee
git config --global user.name DrPl4gue <22fmif.m.voronin@std.npu.edu.ua>
2023-04-24 22:41:01 +0300

    11111

---
+++ ProjectOOP5/ProjectOOP5/WeatherStation.cpp
@@ -0,0 +1,7 @@
+#include"ProjectOOP5.h"
+
+namespace WeatherStation {
+	int WeatherStation::generalinfo::avgtemperature = 10;
+	float WeatherStation::generalinfo::atmpressure = 749.5;
+	int WeatherStation::generalinfo::airhum = 31;
+}
